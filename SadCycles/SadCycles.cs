83d9ea37cb07d7e6aa1e9b3aff1100938fd94b49
Coral Smith <coral.smith92@gmail.com>
2016-02-10 19:00:33 +0000

    Returns list of 1 with tests

---
+++ SadCycles/SadCycles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadCycles
+{
+    public static class SadCycle
+    {
+        public static List<int> Generate(int b, int n)
+        {
+            return new List<int> { 1 }; 
+        }
+    }
+}
---
+++ SadCyclesTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SadCycles;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SadCyclesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenBase2Start1Returns1()
+        {
+            AssertContainsAll(new List<int> { 1 }, SadCycle.Generate(2, 1));
+        }
+
+        [TestMethod]
+        public void GivenBase2Start10Returns1()
+        {
+            AssertContainsAll(new List<int> { 1 }, SadCycle.Generate(2, 10));
+        }
+
+        public void AssertContainsAll(IEnumerable<int> expected, IEnumerable<int> actual)
+        {
+            int n1 = expected.Distinct().Count();
+            int n2 = actual.Distinct().Count();
+
+            Assert.AreEqual(n1, n2);
+            Assert.AreEqual(n1, expected.Intersect(actual).Count());
+            Assert.AreEqual(n2, actual.Intersect(expected).Count());
+        }
+    }
+
+
+}
